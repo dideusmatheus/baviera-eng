4eef5e27b2f96b658e3ef7dbb9706f169657d12e
Matheus de Deus Rocha <matheus.rocha@sis-it.com>
2025-02-08 17:11:23 -0300

    Adjust hamburguer menu

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Toggle do menu lateral
-document.getElementById('hamburgerMenu').addEventListener('click', function() {
+document.getElementById('hamburgerMenu').addEventListener('click', function(event) {
+    event.stopPropagation(); // Impede que o clique se propague para o listener do document
     const sideMenu = document.getElementById('sideMenu');
     sideMenu.classList.toggle('open');
 });
@@ -7,7 +8,8 @@ document.getElementById('hamburgerMenu').addEventListener('click', function() {
 // Fechar o menu quando clicar em um item do menu lateral
 const sideMenuOptions = document.querySelectorAll('.side-menu-option');
 sideMenuOptions.forEach(option => {
-    option.addEventListener('click', function() {
+    option.addEventListener('click', function(event) {
+        event.stopPropagation(); // Impede que o clique se propague
         document.getElementById('sideMenu').classList.remove('open');
     });
 });
@@ -17,7 +19,7 @@ document.addEventListener('click', function(event) {
     const sideMenu = document.getElementById('sideMenu');
     const menuButton = document.getElementById('hamburgerMenu');
     
-    // Verifica se o clique foi fora do menu lateral e do botão hamburger
+    // Se o clique for fora do sideMenu e do botão, fecha o menu
     if (!sideMenu.contains(event.target) && !menuButton.contains(event.target)) {
         sideMenu.classList.remove('open');
     }
